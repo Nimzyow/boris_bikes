12cdc096ac2e0b045d4e2417026cf946071f7238
Nimzyow <n_soufiani@hotmail.com>
2020-02-19 11:18:36 +0000

    step 18 done

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,42 @@
 require_relative "./bike"
 
 class DockingStation
+DEFAULT_CAPACITY = 8
+
+  attr_accessor :currently_docked
+  attr_reader :capacity
+
+  def initialize(capacity: DEFAULT_CAPACITY)
+    @currently_docked = []
+    @capacity = capacity
+  end
+
   def release_bike
+  raise "Dock is empty. Please come back again later" if empty?
     return Bike.new
   end
+
+  def dock_bike(bike:)
+    raise "Full, go to another docking station" if full?
+    currently_docked.push(bike)
+  end
+
+  def show_docked_bikes
+    if currently_docked.length == 0
+      "No bikes here currently"
+    elsif currently_docked.length > 0
+      return currently_docked.length
+    end
+  end
+
+  private
+  def full?
+    currently_docked.length >= capacity ? true : false
+  end
+
+  def empty?
+    currently_docked.length == 0 ? true : false
+  end
 end 
+
+puts DockingStation.new.currently_docked.length
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,14 +1,55 @@
 require "docking_station"
 require "bike"
 
-describe DockingStation do
+shared_context "common" do
+  let(:bike) {Bike.new}
+end
 
+describe DockingStation do
+  include_context "common"
   context "methods" do
     it "responds to #release_bike" do
       expect(subject).to respond_to(:release_bike)
     end
+
+    it "#release_bike prevents release if currently_docked.length = 0" do
+      expect{subject.release_bike}.to raise_error
+    end
+
     it "#release_bike" do
+      subject.dock_bike({bike:bike})
       expect(subject.release_bike).to be_an_instance_of(Bike).and respond_to(:working?)
+    end
+
+    it "#dock_bike pushes Bike instance into currently_docked array" do
+      expect{subject.dock_bike({bike:bike})}.to change{subject.currently_docked.length}.by(1)
+    end
+
+    it "#dock_bike prevents push into currently_docked array if = capacity" do
+      8.times {subject.dock_bike({bike:bike})}
+      expect{subject.dock_bike({bike:bike})}.to raise_error
+    end
+
+    it "#show_docked_bikes returns number of bikes on the docking station" do
+      subject.dock_bike({bike:bike})
+      subject.dock_bike({bike:bike})
+      subject.dock_bike({bike:bike})
+      expect(subject.show_docked_bikes).to eq(subject.currently_docked.length).or eq("No bikes here currently")
+    end
+
+  end
+  context "has attributes" do
+    it "currently_docked" do
+      expect(subject).to have_attributes(currently_docked: [])
+    end
+
+    it "capacity with default of 8" do
+      expect(subject).to have_attributes(capacity: 8)
+    end
+
+    it "capacity with custom capcaity" do
+      subject = described_class.new({capacity: 10})
+      expect(subject).to have_attributes(capacity: 10)
     end
   end
 end
